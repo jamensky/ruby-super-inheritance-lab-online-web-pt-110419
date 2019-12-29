3b72a72e24509f69b0c41ea28d2d1d53f9c4e173
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-29 23:44:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,9 @@
 def Student
-   "Hey there! I'm so excited to learn stuff."
 
-end 
+   def hello
+     "Hey there! I'm so excited to learn stuff."
+   end
+    def raise_hand
+      "Pick me!"
+    end
+end
